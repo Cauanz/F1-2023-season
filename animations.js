19433eff3bb0d06f5eb1456a08d157c8080cbea8
Cauan Zelazowski <61758981+Cauanz@users.noreply.github.com>
2023-02-15 15:55:17 -0300

    Animations optimized

--- animations.js
+++ animations.js
@@ -11,186 +11,84 @@ var redbull = document.querySelector(".carrosel9");
 var list = document.querySelector(".pagnav");
 var child = list.querySelectorAll("a");
 
+function reset() {
+  williams.style.display = "none";
+  alfaromeo.style.display = "none";
+  alphatauri.style.display = "none";
+  haas.style.display = "none";
+  mclaren.style.display = "none";
+  astonmartin.style.display = "none";
+  ferrari.style.display = "none";
+  mercedes.style.display = "none";
+  redbull.style.display = "none";
+}
+
+function resetColor(){
+  child[0].style.backgroundColor = "rgb(255, 24, 1)";
+  child[1].style.backgroundColor = "rgb(255, 24, 1)";
+  child[2].style.backgroundColor = "rgb(255, 24, 1)";
+  child[3].style.backgroundColor = "rgb(255, 24, 1)";
+  child[4].style.backgroundColor = "rgb(255, 24, 1)";
+  child[5].style.backgroundColor = "rgb(255, 24, 1)";
+  child[6].style.backgroundColor = "rgb(255, 24, 1)";
+  child[7].style.backgroundColor = "rgb(255, 24, 1)";
+  child[8].style.backgroundColor = "rgb(255, 24, 1)";
+}
+
 function aparece(n){
   if(n === 1){
-    williams.style.display = "none";
-    alfaromeo.style.display = "none";
-    alphatauri.style.display = "none";
+    reset();
     haas.style.display = "block";
-    mclaren.style.display = "none";
-    astonmartin.style.display = "none";
-    ferrari.style.display = "none";
-    mercedes.style.display = "none";
-    redbull.style.display = "none";
 
+    resetColor();
     child[0].style.backgroundColor = "rgb(122, 0, 0)";
-    child[1].style.backgroundColor = "rgb(255, 24, 1)";
-    child[2].style.backgroundColor = "rgb(255, 24, 1)";
-    child[3].style.backgroundColor = "rgb(255, 24, 1)";
-    child[4].style.backgroundColor = "rgb(255, 24, 1)";
-    child[5].style.backgroundColor = "rgb(255, 24, 1)";
-    child[6].style.backgroundColor = "rgb(255, 24, 1)";
-    child[7].style.backgroundColor = "rgb(255, 24, 1)";
-    child[8].style.backgroundColor = "rgb(255, 24, 1)";
   } else if(n === 2){
+    reset();
     williams.style.display = "block";
-    alfaromeo.style.display = "none";
-    alphatauri.style.display = "none";
-    haas.style.display = "none";
-    mclaren.style.display = "none";
-    astonmartin.style.display = "none";
-    ferrari.style.display = "none";
-    mercedes.style.display = "none";
-    redbull.style.display = "none";
 
-    child[0].style.backgroundColor = "rgb(255, 24, 1)";
+    resetColor();
     child[1].style.backgroundColor = "rgb(122, 0, 0)";
-    child[2].style.backgroundColor = "rgb(255, 24, 1)";
-    child[3].style.backgroundColor = "rgb(255, 24, 1)";
-    child[4].style.backgroundColor = "rgb(255, 24, 1)";
-    child[5].style.backgroundColor = "rgb(255, 24, 1)";
-    child[6].style.backgroundColor = "rgb(255, 24, 1)";
-    child[7].style.backgroundColor = "rgb(255, 24, 1)";
-    child[8].style.backgroundColor = "rgb(255, 24, 1)";
   } else if (n === 3){
-    williams.style.display = "none";
+    reset();
     alfaromeo.style.display = "block";
-    alphatauri.style.display = "none";
-    haas.style.display = "none";
-    mclaren.style.display = "none";
-    astonmartin.style.display = "none";
-    ferrari.style.display = "none";
-    mercedes.style.display = "none";
-    redbull.style.display = "none";
 
-    child[0].style.backgroundColor = "rgb(255, 24, 1)";
-    child[1].style.backgroundColor = "rgb(255, 24, 1)";
+  resetColor();
     child[2].style.backgroundColor = "rgb(122, 0, 0)";
-    child[3].style.backgroundColor = "rgb(255, 24, 1)";
-    child[4].style.backgroundColor = "rgb(255, 24, 1)";
-    child[5].style.backgroundColor = "rgb(255, 24, 1)";
-    child[6].style.backgroundColor = "rgb(255, 24, 1)";
-    child[7].style.backgroundColor = "rgb(255, 24, 1)";
-    child[8].style.backgroundColor = "rgb(255, 24, 1)";
   } else if (n === 4){
-    williams.style.display = "none";
-    alfaromeo.style.display = "none";
+    reset();
     alphatauri.style.display = "block";
-    haas.style.display = "none";
-    mclaren.style.display = "none";
-    astonmartin.style.display = "none";
-    ferrari.style.display = "none";
-    mercedes.style.display = "none";
-    redbull.style.display = "none";
 
-    child[0].style.backgroundColor = "rgb(255, 24, 1)";
-    child[1].style.backgroundColor = "rgb(255, 24, 1)";
-    child[2].style.backgroundColor = "rgb(255, 24, 1)";
+    resetColor();
     child[3].style.backgroundColor = "rgb(122, 0, 0)";
-    child[4].style.backgroundColor = "rgb(255, 24, 1)";
-    child[5].style.backgroundColor = "rgb(255, 24, 1)";
-    child[6].style.backgroundColor = "rgb(255, 24, 1)";
-    child[7].style.backgroundColor = "rgb(255, 24, 1)";
-    child[8].style.backgroundColor = "rgb(255, 24, 1)";
   } else if (n === 5){
-    williams.style.display = "none";
-    alfaromeo.style.display = "none";
-    alphatauri.style.display = "none";
-    haas.style.display = "none";
+    reset();
     mclaren.style.display = "block";
-    astonmartin.style.display = "none";
-    ferrari.style.display = "none";
-    mercedes.style.display = "none";
-    redbull.style.display = "none";
 
-    child[0].style.backgroundColor = "rgb(255, 24, 1)";
-    child[1].style.backgroundColor = "rgb(255, 24, 1)";
-    child[2].style.backgroundColor = "rgb(255, 24, 1)";
-    child[3].style.backgroundColor = "rgb(255, 24, 1)";
+    resetColor();
     child[4].style.backgroundColor = "rgb(122, 0, 0)";
-    child[5].style.backgroundColor = "rgb(255, 24, 1)";
-    child[6].style.backgroundColor = "rgb(255, 24, 1)";
-    child[7].style.backgroundColor = "rgb(255, 24, 1)";
-    child[8].style.backgroundColor = "rgb(255, 24, 1)";
   } else if (n === 6){
-    williams.style.display = "none";
-    alfaromeo.style.display = "none";
-    alphatauri.style.display = "none";
-    haas.style.display = "none";
-    mclaren.style.display = "none";
+    reset();
     astonmartin.style.display = "block";
-    ferrari.style.display = "none";
-    mercedes.style.display = "none";
-    redbull.style.display = "none";
 
-    child[0].style.backgroundColor = "rgb(255, 24, 1)";
-    child[1].style.backgroundColor = "rgb(255, 24, 1)";
-    child[2].style.backgroundColor = "rgb(255, 24, 1)";
-    child[3].style.backgroundColor = "rgb(255, 24, 1)";
-    child[4].style.backgroundColor = "rgb(255, 24, 1)";
+    resetColor();
     child[5].style.backgroundColor = "rgb(122, 0, 0)";
-    child[6].style.backgroundColor = "rgb(255, 24, 1)";
-    child[7].style.backgroundColor = "rgb(255, 24, 1)";
-    child[8].style.backgroundColor = "rgb(255, 24, 1)";
   } else if (n === 7){
-    williams.style.display = "none";
-    alfaromeo.style.display = "none";
-    alphatauri.style.display = "none";
-    haas.style.display = "none";
-    mclaren.style.display = "none";
-    astonmartin.style.display = "none";
+    reset();
     ferrari.style.display = "block";
-    mercedes.style.display = "none";
-    redbull.style.display = "none";
-    
-    child[0].style.backgroundColor = "rgb(255, 24, 1)";
-    child[1].style.backgroundColor = "rgb(255, 24, 1)";
-    child[2].style.backgroundColor = "rgb(255, 24, 1)";
-    child[3].style.backgroundColor = "rgb(255, 24, 1)";
-    child[4].style.backgroundColor = "rgb(255, 24, 1)";
-    child[5].style.backgroundColor = "rgb(255, 24, 1)";
-    child[6].style.backgroundColor = "rgb(122, 0, 0)";
-    child[7].style.backgroundColor = "rgb(255, 24, 1)";
-    child[8].style.backgroundColor = "rgb(255, 24, 1)";
-  } else if (n === 8){
-    williams.style.display = "none";
-    alfaromeo.style.display = "none";
-    alphatauri.style.display = "none";
-    haas.style.display = "none";
-    mclaren.style.display = "none";
-    astonmartin.style.display = "none";
-    ferrari.style.display = "none";
-    mercedes.style.display = "block";
-    redbull.style.display = "none";
 
-    child[0].style.backgroundColor = "rgb(255, 24, 1)";
-    child[1].style.backgroundColor = "rgb(255, 24, 1)";
-    child[2].style.backgroundColor = "rgb(255, 24, 1)";
-    child[3].style.backgroundColor = "rgb(255, 24, 1)";
-    child[4].style.backgroundColor = "rgb(255, 24, 1)";
-    child[5].style.backgroundColor = "rgb(255, 24, 1)";
-    child[6].style.backgroundColor = "rgb(255, 24, 1)";
+    resetColor();
+    child[6].style.backgroundColor = "rgb(122, 0, 0)";
+  } else if (n === 8){
+    reset();
+    mercedes.style.display = "block";
+
+    resetColor();
     child[7].style.backgroundColor = "rgb(122, 0, 0)";
-    child[8].style.backgroundColor = "rgb(255, 24, 1)";
   } else {
-    williams.style.display = "none";
-    alfaromeo.style.display = "none";
-    alphatauri.style.display = "none";
-    haas.style.display = "none";
-    mclaren.style.display = "none";
-    astonmartin.style.display = "none";
-    ferrari.style.display = "none";
-    mercedes.style.display = "none";
+    reset();
     redbull.style.display = "block";
 
-    child[0].style.backgroundColor = "rgb(255, 24, 1)";
-    child[1].style.backgroundColor = "rgb(255, 24, 1)";
-    child[2].style.backgroundColor = "rgb(255, 24, 1)";
-    child[3].style.backgroundColor = "rgb(255, 24, 1)";
-    child[4].style.backgroundColor = "rgb(255, 24, 1)";
-    child[5].style.backgroundColor = "rgb(255, 24, 1)";
-    child[6].style.backgroundColor = "rgb(255, 24, 1)";
-    child[7].style.backgroundColor = "rgb(255, 24, 1)";
+    resetColor();
     child[8].style.backgroundColor = "rgb(122, 0, 0)";
   }
 }
