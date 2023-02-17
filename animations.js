a3687a7b38b704e3bf1bbe724efb6549470cc6b5
Cauan Zelazowski <61758981+Cauanz@users.noreply.github.com>
2023-02-17 15:28:25 -0300

    Mobile menu fixed

--- animations.js
+++ animations.js
@@ -11,6 +11,9 @@ var redbull = document.querySelector(".carrosel9");
 var list = document.querySelector(".pagnav");
 var child = list.querySelectorAll("a");
 
+var mobileList = document.querySelector(".mobile-pagnav");
+var mobileChild = mobileList.querySelectorAll("a");
+
 function reset() {
   williams.style.display = "none";
   alfaromeo.style.display = "none";
@@ -33,6 +36,16 @@ function resetColor(){
   child[6].style.backgroundColor = "rgb(255, 24, 1)";
   child[7].style.backgroundColor = "rgb(255, 24, 1)";
   child[8].style.backgroundColor = "rgb(255, 24, 1)";
+
+  mobileChild[0].style.backgroundColor = "rgb(255, 24, 1)";
+  mobileChild[1].style.backgroundColor = "rgb(255, 24, 1)";
+  mobileChild[2].style.backgroundColor = "rgb(255, 24, 1)";
+  mobileChild[3].style.backgroundColor = "rgb(255, 24, 1)";
+  mobileChild[4].style.backgroundColor = "rgb(255, 24, 1)";
+  mobileChild[5].style.backgroundColor = "rgb(255, 24, 1)";
+  mobileChild[6].style.backgroundColor = "rgb(255, 24, 1)";
+  mobileChild[7].style.backgroundColor = "rgb(255, 24, 1)";
+  mobileChild[8].style.backgroundColor = "rgb(255, 24, 1)";
 }
 
 function aparece(n){
@@ -42,54 +55,63 @@ function aparece(n){
 
     resetColor();
     child[0].style.backgroundColor = "rgb(122, 0, 0)";
+    mobileChild[0].style.backgroundColor = "rgb(122, 0, 0)";
   } else if(n === 2){
     reset();
     williams.style.display = "block";
 
     resetColor();
     child[1].style.backgroundColor = "rgb(122, 0, 0)";
+    mobileChild[1].style.backgroundColor = "rgb(122, 0, 0)";
   } else if (n === 3){
     reset();
     alfaromeo.style.display = "block";
 
   resetColor();
     child[2].style.backgroundColor = "rgb(122, 0, 0)";
+    mobileChild[2].style.backgroundColor = "rgb(122, 0, 0)";
   } else if (n === 4){
     reset();
     alphatauri.style.display = "block";
 
     resetColor();
     child[3].style.backgroundColor = "rgb(122, 0, 0)";
+    mobileChild[3].style.backgroundColor = "rgb(122, 0, 0)";
   } else if (n === 5){
     reset();
     mclaren.style.display = "block";
 
     resetColor();
     child[4].style.backgroundColor = "rgb(122, 0, 0)";
+    mobileChild[4].style.backgroundColor = "rgb(122, 0, 0)";
   } else if (n === 6){
     reset();
     astonmartin.style.display = "block";
 
     resetColor();
     child[5].style.backgroundColor = "rgb(122, 0, 0)";
+    mobileChild[5].style.backgroundColor = "rgb(122, 0, 0)";
   } else if (n === 7){
     reset();
     ferrari.style.display = "block";
 
     resetColor();
     child[6].style.backgroundColor = "rgb(122, 0, 0)";
+    mobileChild[6].style.backgroundColor = "rgb(122, 0, 0)";
   } else if (n === 8){
     reset();
     mercedes.style.display = "block";
 
     resetColor();
     child[7].style.backgroundColor = "rgb(122, 0, 0)";
+    mobileChild[7].style.backgroundColor = "rgb(122, 0, 0)";
   } else {
     reset();
     redbull.style.display = "block";
 
     resetColor();
     child[8].style.backgroundColor = "rgb(122, 0, 0)";
+    mobileChild[8].style.backgroundColor = "rgb(122, 0, 0)";
   }
 }
 
@@ -104,5 +126,4 @@ function closemenu() {
 };
 var x = window.matchMedia("(max-width: 480px)");
 closemenu();
-x.addEventListener(closemenu);
 
