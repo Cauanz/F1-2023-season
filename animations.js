3adb36a166de2cb4c38906d078afda6cba0ee639
Cauan Zelazowski <61758981+Cauanz@users.noreply.github.com>
2023-03-14 20:01:53 -0300

    Simplified functions

--- animations.js
+++ animations.js
@@ -10,117 +10,33 @@ var child = list.querySelectorAll("a");
 var mobileList = document.querySelector(".mobile-pagnav");
 var mobileChild = mobileList.querySelectorAll("a");
 
+
 function reset() {
-  carrosel[0].style.display = "none";
-  carrosel[1].style.display = "none";
-  carrosel[2].style.display = "none";
-  carrosel[3].style.display = "none";
-  carrosel[4].style.display = "none";
-  carrosel[5].style.display = "none";
-  carrosel[6].style.display = "none";
-  carrosel[7].style.display = "none";
-  carrosel[8].style.display = "none";
-  carrosel[9].style.display = "none";
-}
-
-function resetColor(){
-  child[0].style.backgroundColor = "rgb(255, 24, 1)";
-  child[1].style.backgroundColor = "rgb(255, 24, 1)";
-  child[2].style.backgroundColor = "rgb(255, 24, 1)";
-  child[3].style.backgroundColor = "rgb(255, 24, 1)";
-  child[4].style.backgroundColor = "rgb(255, 24, 1)";
-  child[5].style.backgroundColor = "rgb(255, 24, 1)";
-  child[6].style.backgroundColor = "rgb(255, 24, 1)";
-  child[7].style.backgroundColor = "rgb(255, 24, 1)";
-  child[8].style.backgroundColor = "rgb(255, 24, 1)";
-  child[9].style.backgroundColor = "rgb(255, 24, 1)";
-
-  mobileChild[0].style.backgroundColor = "rgb(255, 24, 1)";
-  mobileChild[1].style.backgroundColor = "rgb(255, 24, 1)";
-  mobileChild[2].style.backgroundColor = "rgb(255, 24, 1)";
-  mobileChild[3].style.backgroundColor = "rgb(255, 24, 1)";
-  mobileChild[4].style.backgroundColor = "rgb(255, 24, 1)";
-  mobileChild[5].style.backgroundColor = "rgb(255, 24, 1)";
-  mobileChild[6].style.backgroundColor = "rgb(255, 24, 1)";
-  mobileChild[7].style.backgroundColor = "rgb(255, 24, 1)";
-  mobileChild[8].style.backgroundColor = "rgb(255, 24, 1)";
-  mobileChild[9].style.backgroundColor = "rgb(255, 24, 1)";
-}
-
-function aparece(n){
-  if(n === 1){
-    reset();
-    carrosel[0].style.display = "block";
-
-    resetColor();
-    child[0].style.backgroundColor = "rgb(122, 0, 0)";
-    mobileChild[0].style.backgroundColor = "rgb(122, 0, 0)";
-  } else if(n === 2){
-    reset();
-    carrosel[1].style.display = "block";
-
-    resetColor();
-    child[1].style.backgroundColor = "rgb(122, 0, 0)";
-    mobileChild[1].style.backgroundColor = "rgb(122, 0, 0)";
-  } else if (n === 3){
-    reset();
-    carrosel[2].style.display = "block";
-
-  resetColor();
-    child[2].style.backgroundColor = "rgb(122, 0, 0)";
-    mobileChild[2].style.backgroundColor = "rgb(122, 0, 0)";
-  } else if (n === 4){
-    reset();
-    carrosel[3].style.display = "block";
-
-    resetColor();
-    child[3].style.backgroundColor = "rgb(122, 0, 0)";
-    mobileChild[3].style.backgroundColor = "rgb(122, 0, 0)";
-  } else if (n === 5){
-    reset();
-    carrosel[4].style.display = "block";
-
-    resetColor();
-    child[4].style.backgroundColor = "rgb(122, 0, 0)";
-    mobileChild[4].style.backgroundColor = "rgb(122, 0, 0)";
-  } else if (n === 6){
-    reset();
-    carrosel[5].style.display = "block";
-
-    resetColor();
-    child[5].style.backgroundColor = "rgb(122, 0, 0)";
-    mobileChild[5].style.backgroundColor = "rgb(122, 0, 0)";
-  } else if (n === 7){
-    reset();
-    carrosel[6].style.display = "block";
-
-    resetColor();
-    child[6].style.backgroundColor = "rgb(122, 0, 0)";
-    mobileChild[6].style.backgroundColor = "rgb(122, 0, 0)";
-  } else if (n === 8){
-    reset();
-    carrosel[7].style.display = "block";
-
-    resetColor();
-    child[7].style.backgroundColor = "rgb(122, 0, 0)";
-    mobileChild[7].style.backgroundColor = "rgb(122, 0, 0)";
-  } else if (n === 9) {
-    reset();
-    carrosel[8].style.display = "block";
-
-    resetColor();
-    child[8].style.backgroundColor = "rgb(122, 0, 0)";
-    mobileChild[8].style.backgroundColor = "rgb(122, 0, 0)";
-  } else {
-    reset();
-    carrosel[9].style.display = "block";
-
-    resetColor();
-    child[9].style.backgroundColor = "rgb(122, 0, 0)";
-    mobileChild[9].style.backgroundColor = "rgb(122, 0, 0)";
+  for (let i = 0; i < carrosel.length; i++) {
+    carrosel[i].style.display = "none";
   }
 }
 
+function resetColor() {
+  for (var i = 0; i < child.length; i++) {
+    child[i].style.backgroundColor = "rgb(255, 24, 1)";
+  }
+
+  for (var i = 0; i < mobileChild.length; i++) {
+    mobileChild[i].style.backgroundColor = "rgb(255, 24, 1)";
+  }
+}
+
+function aparece(n){
+  const colors = ["rgb(122, 0, 0)"];
+  
+  reset();
+  carrosel[n-1].style.display = "block";
+
+  resetColor();
+  child[n-1].style.backgroundColor = colors[0];
+  mobileChild[n-1].style.backgroundColor = colors[0];
+}
 
 function closemenu() {
   if (x.matches) { // If media query matches
