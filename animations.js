33cf7fa30f501db9325c17c55a2f2e858219aa21
Cauan Zelazowski <61758981+Cauanz@users.noreply.github.com>
2023-03-14 19:24:15 -0300

    Simplificação das variaveis usando um Loop

--- animations.js
+++ animations.js
@@ -1,13 +1,8 @@
-var haas = document.querySelector(".carrosel1");
-var williams = document.querySelector(".carrosel2");
-var alfaromeo = document.querySelector(".carrosel3");
-var alphatauri = document.querySelector(".carrosel4");
-var mclaren = document.querySelector(".carrosel5");
-var astonmartin = document.querySelector(".carrosel6");
-var ferrari = document.querySelector(".carrosel7");
-var mercedes = document.querySelector(".carrosel8");
-var redbull = document.querySelector(".carrosel9");
-var alpine = document.querySelector(".carrosel10");
+var carrosel = [];
+
+for (var i = 1; i <= 10; i++) {
+  carrosel.push(document.querySelector(".carrosel" + i));
+}
 
 var list = document.querySelector(".pagnav");
 var child = list.querySelectorAll("a");
@@ -16,16 +11,16 @@ var mobileList = document.querySelector(".mobile-pagnav");
 var mobileChild = mobileList.querySelectorAll("a");
 
 function reset() {
-  williams.style.display = "none";
-  alfaromeo.style.display = "none";
-  alphatauri.style.display = "none";
-  haas.style.display = "none";
-  mclaren.style.display = "none";
-  astonmartin.style.display = "none";
-  ferrari.style.display = "none";
-  mercedes.style.display = "none";
-  redbull.style.display = "none";
-  alpine.style.display = "none";
+  carrosel[0].style.display = "none";
+  carrosel[1].style.display = "none";
+  carrosel[2].style.display = "none";
+  carrosel[3].style.display = "none";
+  carrosel[4].style.display = "none";
+  carrosel[5].style.display = "none";
+  carrosel[6].style.display = "none";
+  carrosel[7].style.display = "none";
+  carrosel[8].style.display = "none";
+  carrosel[9].style.display = "none";
 }
 
 function resetColor(){
@@ -55,70 +50,70 @@ function resetColor(){
 function aparece(n){
   if(n === 1){
     reset();
-    haas.style.display = "block";
+    carrosel[0].style.display = "block";
 
     resetColor();
     child[0].style.backgroundColor = "rgb(122, 0, 0)";
     mobileChild[0].style.backgroundColor = "rgb(122, 0, 0)";
   } else if(n === 2){
     reset();
-    williams.style.display = "block";
+    carrosel[1].style.display = "block";
 
     resetColor();
     child[1].style.backgroundColor = "rgb(122, 0, 0)";
     mobileChild[1].style.backgroundColor = "rgb(122, 0, 0)";
   } else if (n === 3){
     reset();
-    alfaromeo.style.display = "block";
+    carrosel[2].style.display = "block";
 
   resetColor();
     child[2].style.backgroundColor = "rgb(122, 0, 0)";
     mobileChild[2].style.backgroundColor = "rgb(122, 0, 0)";
   } else if (n === 4){
     reset();
-    alphatauri.style.display = "block";
+    carrosel[3].style.display = "block";
 
     resetColor();
     child[3].style.backgroundColor = "rgb(122, 0, 0)";
     mobileChild[3].style.backgroundColor = "rgb(122, 0, 0)";
   } else if (n === 5){
     reset();
-    mclaren.style.display = "block";
+    carrosel[4].style.display = "block";
 
     resetColor();
     child[4].style.backgroundColor = "rgb(122, 0, 0)";
     mobileChild[4].style.backgroundColor = "rgb(122, 0, 0)";
   } else if (n === 6){
     reset();
-    astonmartin.style.display = "block";
+    carrosel[5].style.display = "block";
 
     resetColor();
     child[5].style.backgroundColor = "rgb(122, 0, 0)";
     mobileChild[5].style.backgroundColor = "rgb(122, 0, 0)";
   } else if (n === 7){
     reset();
-    ferrari.style.display = "block";
+    carrosel[6].style.display = "block";
 
     resetColor();
     child[6].style.backgroundColor = "rgb(122, 0, 0)";
     mobileChild[6].style.backgroundColor = "rgb(122, 0, 0)";
   } else if (n === 8){
     reset();
-    mercedes.style.display = "block";
+    carrosel[7].style.display = "block";
 
     resetColor();
     child[7].style.backgroundColor = "rgb(122, 0, 0)";
     mobileChild[7].style.backgroundColor = "rgb(122, 0, 0)";
   } else if (n === 9) {
     reset();
-    redbull.style.display = "block";
+    carrosel[8].style.display = "block";
 
     resetColor();
     child[8].style.backgroundColor = "rgb(122, 0, 0)";
     mobileChild[8].style.backgroundColor = "rgb(122, 0, 0)";
   } else {
     reset();
-    alpine.style.display = "block";
+    carrosel[9].style.display = "block";
 
     resetColor();
     child[9].style.backgroundColor = "rgb(122, 0, 0)";
