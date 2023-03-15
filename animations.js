e13be3bde5d1020bec047f314eb1d4b64cb058ba
Cauan Zelazowski <61758981+Cauanz@users.noreply.github.com>
2023-03-15 13:40:28 -0300

    Test/NewCarousel/SlickCarousel

--- animations.js
+++ animations.js
@@ -48,3 +48,44 @@ function closemenu() {
 };
 var x = window.matchMedia("(max-width: 480px)");
 closemenu();
+
+
+$(document).ready(function(){
+  $('.carrosel').slick({
+    dots: true,
+    infinite: true,
+    arrows: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+
+    responsive: [
+      {
+          breakpoint: 1024,
+          settings: {
+              slidesToShow: 1,
+              arrows: false
+          }
+      },
+      {
+          breakpoint: 600,
+          settings: {
+              slidesToShow: 1,
+              arrows: false
+          }
+      },
+      {
+          breakpoint: 480,
+          settings: {
+              slidesToShow: 1,
+              arrows: false
+          }
+      }
+  
+  ]
+  });
+
+
+});
+
